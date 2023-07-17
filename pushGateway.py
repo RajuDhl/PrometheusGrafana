2e550da47b7b28c40abfef70a78b147dffdae6bb
RajuD <33144614+RajuDhl@users.noreply.github.com>
2023-07-18 07:20:51 +1000

    Demonstrate pushGateway Updated installation guide Other changes

--- pushGateway.py
+++ pushGateway.py
@@ -1,47 +1,45 @@
 """
 This code snippet demonstrates how to create and update a Gauge metric using the Prometheus Python client library.
 """
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, CollectorRegistry, push_to_gateway
 import time
 
 # initialize start_time variable to record function starting time
 start_time = time.time()
 
 
-def test_gauge(value):
+def test_push_to_gateway(value):
     """
-    Start an HTTP server on port 8000 and set a gauge value of 100 to be sent on the server
     Args:
          gauge_metric: name of the gauge metric being created
+         registry: name of the registry
     Returns:
        Returns a success message after successfully running for 100 seconds
     Raises:
           Server Creation Error with related error message
     """
     try:
+        # CollectorRegistry class to manage the registry of metrics.
+        registry = CollectorRegistry()
+
         # Create a Gauge metric
         gauge_metric = Gauge(
             'my_gauge_metric',
             'Test gauge metric',
-            ['label_name1', 'label_name2']
-        )
-
-        # Start an HTTP server on port 8001
-        start_http_server(8001)
+            registry=registry)
 
         # Update the gauge metric value
         while True:
-            gauge_metric.labels(label_name1='value1', label_name2='value2').set(value)
-            time.sleep(10000)  # Sleep for 5 second
+            gauge_metric.set(value)
+            push_to_gateway('localhost:9091', job='test_job', registry=registry)
+            time.sleep(5)  # Sleep for 5 second
 
             # exit the loop after 100 seconds
             if time.time() - start_time >= 100:
                 break
-
-            return "Successfully created prometheus server on port 8000"
-
+        return "Bla Bla Bla"
     except Exception as e:
-        print(str(e))
+        print("Exception occurred =>", str(e))
 
 
-print(test_gauge(1))
+print(test_push_to_gateway(100))
